dd69747eb3058a9e686dadc3646ff7f046330435
Ernst <rasmusernst@gmail.com>
2018-01-02 14:04:51 +0100

    added front page styling. Need to finish styling front page and fix busted logo graphic.

--- src/containers/Frontpage/index.js
+++ src/containers/Frontpage/index.js
@@ -11,10 +11,12 @@ import { FormControl } from 'material-ui/Form'
 import Input, { InputLabel } from 'material-ui/Input'
 import { withStyles } from 'material-ui/styles'
 import Hidden from 'material-ui/Hidden'
+import Icon from 'material-ui/Icon'
 
 import { actions as authActions } from '../../stores/auth'
 
 import logo from './zenziot_logo.svg'
+import logoWhite from './zenziot_logo_white.svg'
 import customClasses from './style.scss'
 
 const styles = theme => ({
@@ -23,7 +25,7 @@ const styles = theme => ({
 			backgroundColor: theme.palette.common.lightWhite + '!important',
 		},
 	},
-	inputLabelFocused: {
+	inputContrast: {
 		color: theme.palette.common.white,
 	},
 	inputInkbar: {
@@ -67,30 +69,92 @@ class Frontpage extends PureComponent {
 			<Grid container spacing={0} >
 
 				<Hidden mdDown>
-					<Grid item xs={12} md={8} className={customClasses.wrapper}>
+					<Grid item xs={12} md={8} >
 						<Grid container spacing={24} alignItems='center' justify='center' className={customClasses.infoContainer}>
-							<Grid item xs={5}>
-								<div className={customClasses.zenziotLogoWrapper}>
-									<img src={logo} alt='ZenzIOT logo' />
-								</div>
-								<Typography type='headline' align='center' gutterBottom>
-									Sporingsportal
-								</Typography>
-								<Typography type='body1' align='center' gutterBottom>
-									Overvåg og administrer din GPS tracker. Opsæt alarmer og modtag besked på mail og SMS, hvis en GPS tracker bevæger sig udenfor et tilladt tidspunkt eller område.
-								</Typography>
+							<Grid item xs={12}>
+								<Grid container spacing={24} direction='column' alignItems='center' justify='center'>
+
+									<Grid item xs={6}>
+										<div className={customClasses.zenziotLogo}>
+											<img src={logo} alt='ZenzIOT logo' />
+										</div>
+
+										<Typography type='headline' align='center' gutterBottom>
+										Sporingsportal
+										</Typography>
+
+										<Typography type='body1' align='center' gutterBottom>
+										Overvåg og administrer din GPS tracker.
+										Opsæt alarmer og modtag besked på mail og SMS,
+										hvis en GPS tracker bevæger sig udenfor et tilladt tidspunkt
+										eller område.
+										</Typography>
+									</Grid>
+
+									<Grid item xs={6}>
+										<Grid container spacing={24} alignItems='center' justify='center'>
+
+											<Grid item xs={6} >
+												<Typography type='headline' align='center' gutterBottom>
+													<Icon className={customClasses.icon} color='primary' >monetization_on</Icon>
+												</Typography>
+												<Typography type='body1' align='center' gutterBottom>
+													Uhørt billigt
+												</Typography>
+											</Grid>
+
+											<Grid item xs={6} >
+												<Typography type='headline' align='center' gutterBottom>
+													<Icon className={customClasses.icon} color='primary' >alarm_on</Icon>
+												</Typography>
+												<Typography type='body1' align='center' gutterBottom>
+													Hurtigt i gang
+												</Typography>
+											</Grid>
+
+											<Grid item xs={6} >
+												<Typography type='headline' align='center' gutterBottom>
+													<Icon className={customClasses.icon} color='primary' >battery_charging_full</Icon>
+												</Typography>
+												<Typography type='body1' align='center' gutterBottom>
+													Lang batterilevetid
+												</Typography>
+											</Grid>
+
+											<Grid item xs={6} >
+												<Typography type='headline' align='center' gutterBottom>
+													<Icon className={customClasses.icon} color='primary' >language</Icon>
+												</Typography>
+												<Typography type='body1' align='center' gutterBottom>
+													Sporingsportal
+												</Typography>
+											</Grid>
+
+										</Grid>
+									</Grid>
+								</Grid>
 							</Grid>
 						</Grid>
 					</Grid>
 				</Hidden>
 
-				<Grid item xs={12} md={4} className={customClasses.loginWrapper}>
+				<Grid item xs={12} md={4} >
 					<Grid container alignItems='center' justify='center' className={customClasses.loginContainer}>
 						<Grid item xs={6}>
-							<Grid container direction='column' alignItems='center'>
+							<Grid container direction='column' alignItems='center' justify='center' >
 
 								<Grid item xs={12}>
-									<Typography type='headline' gutterBottom>
+									<div className={customClasses.zenziotLogoWhite}>
+										<img src={logoWhite} alt='Hvidt ZenzIOT logo' />
+									</div>
+
+									<Typography type='body1' align='center' gutterBottom classes={{	root: classes.inputContrast }}>
+									Sporingsportal
+									</Typography>
+								</Grid>
+
+								<Grid item xs={12}>
+									<Typography type='headline' gutterBottom classes={{	root: classes.inputContrast }}>
 										Log ind
 									</Typography>
 								</Grid>
@@ -98,7 +162,7 @@ class Frontpage extends PureComponent {
 								<Grid item xs={12}>
 									<FormControl >
 
-										<InputLabel	FormControlClasses={{	root: classes.inputLabelFocused }} htmlFor='email' >
+										<InputLabel	FormControlClasses={{	root: classes.inputContrast }} htmlFor='email' >
 											E-mail Addresse
 										</InputLabel>
 
@@ -118,7 +182,7 @@ class Frontpage extends PureComponent {
 									<Grid item xs={12}>
 										<FormControl margin='normal'>
 
-											<InputLabel	FormControlClasses={{	root: classes.inputLabelFocused }} htmlFor='password' >
+											<InputLabel	FormControlClasses={{	root: classes.inputContrast }} htmlFor='password' >
 												Kodeord
 											</InputLabel>
 
